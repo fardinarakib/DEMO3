d587a69420eab69663d52ed9265198d47b6e69f2
Fardina rakib <fardina.rakib@yahoo.com>
2024-04-10 15:38:49 -0600

    fxml features added

--- src/main/java/ca/ucalgary/syeda/rakib/demo3gui/Main.java
+++ src/main/java/ca/ucalgary/syeda/rakib/demo3gui/Main.java
@@ -1,9 +1,26 @@
 package ca.ucalgary.syeda.rakib.demo3gui;
 
-import ca.ucalgary.syeda.rakib.demo3gui.Menu;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
-public class Main {
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+        Parent root = FXMLLoader.load(getClass().getResource("Main.fxml"));
+        Scene scene = new Scene(root);
+
+
+        primaryStage.setTitle("Student Data Management System");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
     public static void main(String[] args) {
-        Menu.menuLoop();
+        launch(args);
     }
 }
